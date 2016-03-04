d68f27581b36eff4935aec92efe7c2296a8b11f4
Adrien Coppens <qdrien@gmail.com>
2016-03-04 22:33:10 +0100

    page skeleton

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <title>zdqcsclk</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <link rel="icon" type="image/png" href="./ressources/images/icon.png" />
+    <script src="js/jquery-2.0.3.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/html5shiv-printshiv.js"></script>
+    <script src="js/html5shiv.js"></script>
+    <meta charset="utf-8">
+</head>
+<body>
+<div id="container">
+    <?php include "./includes/accountBar.php"; ?>
+<?php include "./includes/header.php"; ?>
+<?php include "./includes/nav.php"; ?>
+<div id="content">
+
+</div>
+<?php include"./includes/footer.php" ?>
+</div>
+</body>
+</html>
