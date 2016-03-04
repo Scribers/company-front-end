70294ecf59e011876529ddceb14d8ee0b1835b71
Adrien Coppens <qdrien@gmail.com>
2016-03-04 23:12:57 +0100

    translated to french because WHY NOT

--- includes/nav.php
+++ includes/nav.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="nav">
     <ul class="nav navbar-nav">
-        <li id="index"><a href="index.php">Home</a></li>
-        <li id="newOffer"><a href="createOffer.php">New offer</a></li>
-        <li id="myOffers"><a href="myOffers.php">My offers</a></li>
-        <li id="profile"><a href="profile.php">My profile</a></li>
-        <li id="login"><a href="login.php">Log in</a></li>
-        <li id="signup"><a href="signup.php">Sign up</a></li>
+        <li id="index"><a href="index.php">Accueil</a></li>
+        <li id="newOffer"><a href="createOffer.php">Nouvelle offre</a></li>
+        <li id="myOffers"><a href="myOffers.php">Mes offres</a></li>
+        <li id="profile"><a href="profile.php">Mon profil</a></li>
+        <li id="login"><a href="login.php">Se connecter</a></li>
+        <li id="signup"><a href="signup.php">S'inscrire</a></li>
         <li id="apk"><a href="https://tournamentmanager.blob.core.windows.net/app/tournament-manager.apk">
                 <img src="./ressources/images/android.png" style="width: 10%;margin-top: -3px;" alt="Apk"> Télécharger l'application Android</a></li>
     </ul>
