bfe48713e1445d46b29a0a0652ecfc9e549e7a91
Adrien Coppens <qdrien@gmail.com>
2016-03-05 00:47:04 +0100

    links now work

--- includes/nav.php
+++ includes/nav.php
@@ -6,14 +6,14 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li><a id="index" href="#">Accueil</a></li>
-                <li><a id="createOffer" href="#">Nouvelle offre</a></li>
-                <li><a id="myOffers" href="#">Mes offres</a></li>
-                <li><a id="profile" href="#">Mon profil</a></li>
+                <li><a id="index" href="index.php">Accueil</a></li>
+                <li><a id="createOffer" href="createOffer.php">Nouvelle offre</a></li>
+                <li><a id="myOffers" href="myOffers.php">Mes offres</a></li>
+                <li><a id="profile" href="profile.php">Mon profil</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="./">S'inscrire <span class="sr-only">(current)</span></a></li>
-                <li><a>Se connecter</a></li>
+                <li class="active"><a href="signup.php">S'inscrire <span class="sr-only">(current)</span></a></li>
+                <li><a href="login.php">Se connecter</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div><!--/.container-fluid -->
