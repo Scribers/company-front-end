23b2c4da88930d06ec9e8bf87a4d3671fe5fa8da
Adrien Coppens <qdrien@gmail.com>
2016-03-05 14:56:51 +0100

    !

--- index.php
+++ index.php
@@ -13,19 +13,8 @@
 <?php include("includes/nav.php") ?>
 
     <div class="row">
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
+        <div class="col-md-2"> <img class="img-responsive" src="img/logo.png" alt="Chania"> </div>
+        <div class="col-md-10"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor nibh in mi ultricies, dictum consequat orci consectetur. Integer tincidunt ante et nulla maximus, vitae rutrum erat pulvinar. Fusce non ullamcorper erat, tempus consectetur nibh. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur elementum dapibus mauris ac hendrerit. Pellentesque malesuada commodo urna, sit amet maximus orci gravida quis.</p><p>Nullam tempor eros et eleifend venenatis. Pellentesque sed iaculis mauris. Curabitur tempor arcu quis cursus suscipit. Nam euismod lacinia purus, ac convallis erat convallis at. Fusce dignissim pulvinar lorem nec vehicula. Donec lorem odio, consectetur vitae leo ut, consequat semper erat. </p></div>
     </div>
 
 </div>
@@ -36,5 +25,17 @@
 <script src="js/html5shiv.js"></script>
 <script src="js/active.js"></script>
 <script>setActive("index");</script>
+<script>
+        $.ajax({
+            type: 'GET',
+            url: 'https://bootswatch.com/api/3.json',
+            data: { get_param: 'value' },
+            dataType:'json',
+            success: function (data) {
+                var content = data.content;
+                $('#test').html(content);
+            }
+        });
+</script>
 </body>
 </html>
